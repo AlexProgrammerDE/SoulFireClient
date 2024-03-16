62e5ed7fee973e02aa4d7223aadad07359956d53
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-03-16 22:05:34 +0100

    Add system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,31 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// Prevents an additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let open = CustomMenuItem::new("open".to_string(), "Open SoulFire");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit SoulFire");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(open)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                match id.as_str() {
+                    "open" => {
+                        app.get_window("main").unwrap().show().unwrap();
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
