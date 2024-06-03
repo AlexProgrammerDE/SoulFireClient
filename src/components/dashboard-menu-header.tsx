0d268a82db61ad3f885e13058b82dc6aba874198
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-03 18:13:37 +0200

    Implement saving and loading profiles

--- src/components/dashboard-menu-header.tsx
+++ src/components/dashboard-menu-header.tsx
@@ -6,7 +6,6 @@ import {
   MenubarRadioGroup,
   MenubarRadioItem,
   MenubarSeparator,
-  MenubarShortcut,
   MenubarSub,
   MenubarSubContent,
   MenubarSubTrigger,
@@ -19,19 +18,22 @@ import {AboutPopup} from "@/components/about-popup.tsx";
 import {useContext, useState} from "react";
 import {useNavigate} from "@tanstack/react-router";
 import {ProfileContext} from "@/components/providers/profile-context.tsx";
+import {saveAs} from 'file-saver';
+
+function data2blob(data: string) {
+  const bytes = new Array(data.length);
+  for (let i = 0; i < data.length; i++) {
+    bytes[i] = data.charCodeAt(i);
+  }
+
+  return new Blob([new Uint8Array(bytes)]);
+}
 
 export const DashboardMenuHeader = () => {
   const {theme, setTheme} = useTheme()
   const [aboutOpen, setAboutOpen] = useState(false)
   const navigate = useNavigate()
   const profile = useContext(ProfileContext)
-
-  const loadProfile = async () => {
-  }
-
-  const saveProfile = async () => {
-
-  }
 
   return (
       <>
@@ -44,10 +46,27 @@ export const DashboardMenuHeader = () => {
           <MenubarMenu>
             <MenubarTrigger>File</MenubarTrigger>
             <MenubarContent>
-              <MenubarItem onClick={loadProfile}>
+              <input id="profile-load-input" type="file"
+                     accept=".json"
+                     className="hidden" onInput={e => {
+                const file = (e.target as HTMLInputElement).files?.item(0)
+                if (!file) return
+
+                const reader = new FileReader()
+                reader.onload = () => {
+                  const data = reader.result as string
+                  profile.setProfile(JSON.parse(data))
+                }
+                reader.readAsText(file)
+              }}/>
+              <MenubarItem onClick={() => {
+                document.getElementById("profile-load-input")?.click()
+              }}>
                 Load Profile
               </MenubarItem>
-              <MenubarItem onClick={saveProfile}>
+              <MenubarItem onClick={() => {
+                saveAs(data2blob(JSON.stringify(profile)), "profile.json")
+              }}>
                 Save Profile
               </MenubarItem>
               <MenubarSeparator/>
