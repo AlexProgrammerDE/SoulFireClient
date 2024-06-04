711110ac4c7a9dbbb50ec248caaf77a5b5d5dcea
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-04 18:26:28 +0200

    Add tab completion

--- src/components/command-input.tsx
+++ src/components/command-input.tsx
@@ -1,19 +1,85 @@
 import {Input} from "@/components/ui/input.tsx";
-import {KeyboardEventHandler, useContext} from "react";
+import {KeyboardEventHandler, useContext, useState} from "react";
 import {ServerConnectionContext} from "@/components/providers/server-context.tsx";
 import {CommandServiceClient} from "@/generated/com/soulfiremc/grpc/generated/command.client.ts";
 
+type CompletionState = {
+  lastWritten: string,
+  receivedCompletions: string[] | null,
+  index: number | null
+}
+
 export default function CommandInput() {
   const transport = useContext(ServerConnectionContext)
+  const [completionState, setCompletionState] = useState<CompletionState>({
+    lastWritten: "",
+    receivedCompletions: null,
+    index: null
+  })
 
   const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    const currenTarget = e.currentTarget
     if (e.key === "Enter") {
+      e.preventDefault()
+
+      const currentVal = currenTarget.value
+      currenTarget.value = ""
+
       const commandService = new CommandServiceClient(transport);
       void commandService.executeCommand({
-        command: e.currentTarget.value
+        command: currentVal
       })
+    } else if (e.key === "Tab") {
+      e.preventDefault()
+
+      void handleTabPress(currenTarget.value, currenTarget)
     }
   }
 
-  return <Input placeholder="Enter command" onKeyDown={handleKeyDown}/>
+  const handleTabPress = async (text: string, element: HTMLInputElement) => {
+    const commandService = new CommandServiceClient(transport);
+
+    let completionStateNew: CompletionState
+    if (completionState.receivedCompletions === null || completionState.index === null) {
+      const {response} = await commandService.tabCompleteCommand({
+        command: text
+      })
+
+      completionStateNew = {
+        lastWritten: text,
+        receivedCompletions: response.suggestions,
+        index: 0
+      }
+    } else if (completionState.receivedCompletions.length > 0) {
+      const newIndex = (completionState.index + 1) % completionState.receivedCompletions.length
+      completionStateNew = {
+        lastWritten: text,
+        receivedCompletions: completionState.receivedCompletions,
+        index: newIndex
+      }
+    } else {
+      completionStateNew = {
+        lastWritten: text,
+        receivedCompletions: [],
+        index: null
+      }
+    }
+
+    if (completionStateNew.receivedCompletions!.length > 0) {
+      const split = completionStateNew.lastWritten.split(" ")
+      split[split.length - 1] = completionStateNew.receivedCompletions![completionStateNew.index!]
+
+      element.value = split.join(" ")
+    }
+
+    setCompletionState(completionStateNew)
+  }
+
+  return <Input placeholder="Enter command" onKeyDown={handleKeyDown} onChange={(e) => {
+    setCompletionState({
+      lastWritten: e.currentTarget.value,
+      receivedCompletions: null,
+      index: null
+    })
+  }}/>
 }
