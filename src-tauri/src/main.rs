7a1395fe458e3cd717d2ea9a5748bdb2ae7b47a4
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-07 18:25:12 +0200

    Add basic discord rpc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,8 @@ use std::net::TcpListener;
 use std::str::FromStr;
 use std::sync::atomic::AtomicBool;
 use std::sync::mpsc::{channel, Sender};
+use discord_presence::{Client, Event};
+use discord_presence::models::ActivityButton;
 use mdns_sd::{ServiceDaemon, ServiceEvent};
 use rust_cast::{CastDevice, ChannelMessage};
 use rust_cast::channels::heartbeat::HeartbeatResponse;
@@ -442,8 +444,53 @@ fn create_cast_connection(address: String, port: u16, app_handle: AppHandle, cha
   }
 }
 
+fn load_discord_rpc() {
+  let mut drpc = Client::new(1248603974475583608);
+
+  let _ready = drpc.on_ready(|_ctx| {
+    println!("ready?");
+    ;
+  });
+
+  let _activity_join_request = drpc.on_activity_join_request(|ctx| {
+    println!("Join request: {:?}", ctx.event);
+  });
+
+  let _activity_join = drpc.on_activity_join(|ctx| {
+    println!("Joined: {:?}", ctx.event);
+  });
+
+  let _activity_spectate = drpc.on_activity_spectate(|ctx| {
+    println!("Spectate: {:?}", ctx.event);
+  });
+
+  drpc.start();
+
+  drpc.block_until_event(Event::Ready).unwrap();
+  if let Err(why) = drpc.set_activity(|a| {
+    a.state("Idling")
+      .assets(|ass| {
+        ass.large_image("logo")
+          .large_text("SoulFire logo")
+      })
+      .append_buttons(|button|
+        button
+          .label("Learn more")
+          .url("https://soulfiremc.com")
+      )
+  }) {
+    println!("Failed to set presence: {}", why);
+  }
+
+  drpc.block_on().unwrap();
+}
+
 fn main() {
   env_logger::init();
+
+  thread::spawn(|| {
+    load_discord_rpc()
+  });
 
   let open = CustomMenuItem::new("open".to_string(), "Open SoulFire");
   let quit = CustomMenuItem::new("quit".to_string(), "Quit SoulFire");
