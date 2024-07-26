5ca8d156d1065813700bb2d551fb05f4331fd83c
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-07-26 17:06:47 +0200

    Improve ipc

--- src-tauri/src/discord.rs
+++ src-tauri/src/discord.rs
@@ -1,40 +1,58 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use discord_presence::{Client, Event};
+use log::{error, info};
 
 pub fn load_discord_rpc() {
   let mut drpc = Client::new(1248603974475583608);
 
   let _ready = drpc.on_ready(|_ctx| {
-    println!("ready?");
+    info!("Discord RPC ready!");
+  });
+
+  let _ready = drpc.on_error(|error| {
+    error!("Discord RPC error: {:?}", error);
   });
 
   let _activity_join_request = drpc.on_activity_join_request(|ctx| {
-    println!("Join request: {:?}", ctx.event);
+    info!("Join request: {:?}", ctx.event);
   });
 
   let _activity_join = drpc.on_activity_join(|ctx| {
-    println!("Joined: {:?}", ctx.event);
+    info!("Joined: {:?}", ctx.event);
   });
 
   let _activity_spectate = drpc.on_activity_spectate(|ctx| {
-    println!("Spectate: {:?}", ctx.event);
+    info!("Spectate: {:?}", ctx.event);
   });
 
   drpc.start();
 
+  let start = SystemTime::now();
+  let epoch_secs = start
+    .duration_since(UNIX_EPOCH)
+    .expect("Time went backwards")
+    .as_secs();
   drpc.block_until_event(Event::Ready).unwrap();
   if let Err(why) = drpc.set_activity(|a| {
     a.state("Idling")
-      .assets(|ass| {
-        ass.large_image("logo")
+      .details("Professional bot tool")
+      .timestamps(|timestamps| {
+        timestamps
+          .start(epoch_secs)
+      })
+      .assets(|assets| {
+        assets
+          .large_image("logo")
           .large_text("SoulFire logo")
       })
-      .append_buttons(|button|
-      button
-        .label("Learn more")
-        .url("https://soulfiremc.com")
-      )
+      .append_buttons(|button| {
+        button
+          .label("Learn more")
+          .url("https://soulfiremc.com")
+      })
   }) {
-    println!("Failed to set presence: {}", why);
+    error!("Failed to set presence: {}", why);
   }
 
   drpc.block_on().unwrap();
