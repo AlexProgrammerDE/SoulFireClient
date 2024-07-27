53390f4108940bee7b3e41b78e854f9f5c76d49d
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-07-27 10:22:25 +0200

    Use newer router plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
