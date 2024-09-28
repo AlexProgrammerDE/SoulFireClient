30d4690e182f59f24db0929d6d72fc906bec0d12
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-09-28 09:43:33 +0200

    Keep icons cached longer

--- src/components/dynamic-icon.tsx
+++ src/components/dynamic-icon.tsx
@@ -1,13 +1,27 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { LucideProps } from 'lucide-react';
 import dynamicIconImports from 'lucide-react/dynamicIconImports';
+import loadable from '@loadable/component';
 
 interface IconProps extends Omit<LucideProps, 'ref'> {
   name: keyof typeof dynamicIconImports;
 }
 
+const mappedImports = Object.entries(dynamicIconImports).reduce(
+  (acc, [name, importFn]) => {
+    acc[name as unknown as keyof typeof dynamicIconImports] = loadable(
+      importFn,
+      {
+        cacheKey: () => name,
+      },
+    );
+    return acc;
+  },
+  {} as Record<keyof typeof dynamicIconImports, ReturnType<typeof loadable>>,
+);
+
 const DynamicIcon = ({ name, ...props }: IconProps) => {
-  const LucideIcon = lazy(dynamicIconImports[name]);
+  const LucideIcon = mappedImports[name];
 
   return (
     <Suspense fallback={<div className={props.className} />}>
