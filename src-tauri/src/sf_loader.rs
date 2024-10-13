22daf9703ed3562e602950bbc810fba1fb6d4404
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-10-13 18:34:05 +0200

    Update to 1.14.0

--- src-tauri/src/sf_loader.rs
+++ src-tauri/src/sf_loader.rs
@@ -35,7 +35,7 @@ pub async fn run_integrated_server(
     .starting
     .store(true, std::sync::atomic::Ordering::Relaxed);
 
-  let soul_fire_version = "1.13.1";
+  let soul_fire_version = "1.14.0";
 
   fn send_log<S: Serialize + Clone>(app_handle: &AppHandle, payload: S) {
     app_handle
