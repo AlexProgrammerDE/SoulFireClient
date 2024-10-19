4a6a203a21fa6a81d17cf661ad038ab051adf315
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-10-19 09:56:38 +0200

    Make integrated server code never panic again

--- src-tauri/src/sf_loader.rs
+++ src-tauri/src/sf_loader.rs
@@ -1,15 +1,55 @@
 use crate::utils::{detect_architecture, detect_os, extract_tar_gz, extract_zip, find_random_available_port, get_java_exec_name};
 use log::info;
 use serde::Serialize;
+use sha2::Digest;
 use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
-use sha2::Digest;
 use tauri::async_runtime::Mutex;
 use tauri::{AppHandle, Emitter, Manager};
-use tauri_plugin_shell::process::CommandEvent::Stdout;
 use tauri_plugin_shell::process::CommandChild;
+use tauri_plugin_shell::process::CommandEvent::Stdout;
 use tauri_plugin_shell::ShellExt;
 use tempfile::tempdir;
+
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum SFError {
+  #[error("checksum of downloaded jvm data does not match")]
+  InvalidJvmChecksum,
+  #[error("json field was invalid/not found: {0}")]
+  JsonFieldInvalid(String),
+  #[error("jwt line was invalid")]
+  JwtLineInvalid,
+  #[error("path could not be converted to string")]
+  PathCouldNotBeConverted,
+  #[error("no content length header present")]
+  NoContentLengthHeader,
+  #[error("no port available")]
+  NoPortAvailable,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+  #[error(transparent)]
+  SFError(#[from] SFError),
+  #[error(transparent)]
+  Io(#[from] std::io::Error),
+  #[error(transparent)]
+  Tauri(#[from] tauri::Error),
+  #[error(transparent)]
+  Reqwest(#[from] reqwest::Error),
+  #[error(transparent)]
+  FromUtf8(#[from] std::string::FromUtf8Error),
+}
+
+impl serde::Serialize for Error {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: serde::ser::Serializer,
+  {
+    serializer.serialize_str(self.to_string().as_ref())
+  }
+}
 
 pub struct IntegratedServerState {
   pub starting: Arc<AtomicBool>,
@@ -20,7 +60,7 @@ pub struct IntegratedServerState {
 pub async fn run_integrated_server(
   app_handle: AppHandle,
   integrated_server_state: tauri::State<'_, IntegratedServerState>,
-) -> Result<String, ()> {
+) -> Result<String, Error> {
   if integrated_server_state
     .starting
     .load(std::sync::atomic::Ordering::Relaxed)
@@ -34,13 +74,12 @@ pub async fn run_integrated_server(
 
   let soul_fire_version = "1.14.0";
 
-  fn send_log<S: Serialize + Clone>(app_handle: &AppHandle, payload: S) {
+  fn send_log<S: Serialize + Clone>(app_handle: &AppHandle, payload: S) -> tauri::Result<()> {
     app_handle
       .emit("integrated-server-start-log", payload)
-      .unwrap();
   }
 
-  let app_local_data_dir = app_handle.path().app_local_data_dir().unwrap();
+  let app_local_data_dir = app_handle.path().app_local_data_dir()?;
   let jvm_dir = app_local_data_dir.join("jvm-21");
   if !jvm_dir.exists() {
     let adoptium_os = detect_os();
@@ -48,52 +87,52 @@ pub async fn run_integrated_server(
     let jvm_url = format!("https://api.adoptium.net/v3/assets/latest/21/hotspot?architecture={}&image_type=jre&os={}&vendor=eclipse", adoptium_arch, adoptium_os);
     info!("JVM URL: {}", jvm_url);
 
-    send_log(&app_handle, "Fetching JVM data...");
-    let response = reqwest::get(&jvm_url).await.unwrap();
-    let jvm_json: serde_json::Value = response.json().await.unwrap();
-    let checksum = jvm_json[0]["binary"]["package"]["checksum"].as_str().unwrap();
-    let download_url = jvm_json[0]["binary"]["package"]["link"].as_str().unwrap();
-    let major_version = jvm_json[0]["version"]["major"].as_u64().unwrap();
-    let minor_version = jvm_json[0]["version"]["minor"].as_u64().unwrap();
-    let security_version = jvm_json[0]["version"]["security"].as_u64().unwrap();
-    let build_version = jvm_json[0]["version"]["build"].as_u64().unwrap();
+    send_log(&app_handle, "Fetching JVM data...")?;
+    let response = reqwest::get(&jvm_url).await?;
+    let jvm_json: serde_json::Value = response.json().await?;
+    let checksum = jvm_json[0]["binary"]["package"]["checksum"].as_str().ok_or(SFError::JsonFieldInvalid("binary.package.checksum".to_string()))?;
+    let download_url = jvm_json[0]["binary"]["package"]["link"].as_str().ok_or(SFError::JsonFieldInvalid("binary.package.link".to_string()))?;
+    let major_version = jvm_json[0]["version"]["major"].as_u64().ok_or(SFError::JsonFieldInvalid("version.major".to_string()))?;
+    let minor_version = jvm_json[0]["version"]["minor"].as_u64().ok_or(SFError::JsonFieldInvalid("version.minor".to_string()))?;
+    let security_version = jvm_json[0]["version"]["security"].as_u64().ok_or(SFError::JsonFieldInvalid("version.security".to_string()))?;
+    let build_version = jvm_json[0]["version"]["build"].as_u64().ok_or(SFError::JsonFieldInvalid("version.build".to_string()))?;
     info!("Download URL: {}", download_url);
     let jvm_archive_dir_name = format!(
       "jdk-{}.{}.{}+{}-jre",
       major_version, minor_version, security_version, build_version
     );
 
-    fn send_download_progress(app_handle: &AppHandle, progress: u64, total: u64) {
+    fn send_download_progress(app_handle: &AppHandle, progress: u64, total: u64) -> tauri::Result<()> {
       send_log(
         app_handle,
         format!("Downloading JVM... {}%", progress * 100 / total),
-      );
+      )
     }
 
-    send_download_progress(&app_handle, 0, 1);
-    let mut response = reqwest::get(download_url).await.unwrap();
-    let total_size = response.content_length().unwrap();
+    send_download_progress(&app_handle, 0, 1)?;
+    let mut response = reqwest::get(download_url).await?;
+    let total_size = response.content_length().ok_or(SFError::NoContentLengthHeader)?;
     let mut downloaded_size = 0;
     let mut content = Vec::new();
-    while let Some(chunk) = response.chunk().await.unwrap() {
+    while let Some(chunk) = response.chunk().await? {
       downloaded_size += chunk.len() as u64;
       content.extend_from_slice(&chunk);
-      send_download_progress(&app_handle, downloaded_size, total_size);
+      send_download_progress(&app_handle, downloaded_size, total_size)?;
     }
 
-    send_log(&app_handle, "Verifying sha256 checksum...");
+    send_log(&app_handle, "Verifying sha256 checksum...")?;
     let mut hasher = sha2::Sha256::new();
     hasher.update(&content);
     let hash = hasher.finalize();
     let hash = format!("{:x}", hash);
     if hash != checksum {
-      send_log(&app_handle, "Checksum verification failed");
-      return Err(());
+      send_log(&app_handle, "Checksum verification failed")?;
+      return Err(Error::from(SFError::InvalidJvmChecksum));
     }
 
-    send_log(&app_handle, "Extracting JVM...");
+    send_log(&app_handle, "Extracting JVM...")?;
 
-    let jvm_tmp_dir = tempdir().unwrap();
+    let jvm_tmp_dir = tempdir()?;
     if download_url.ends_with(".tar.gz") {
       extract_tar_gz(&content[..], jvm_tmp_dir.path());
     } else if download_url.ends_with(".zip") {
@@ -102,67 +141,67 @@ pub async fn run_integrated_server(
       panic!("Unsupported JVM archive format");
     }
 
-    std::fs::rename(jvm_tmp_dir.path().join(jvm_archive_dir_name), &jvm_dir).unwrap();
+    std::fs::rename(jvm_tmp_dir.path().join(jvm_archive_dir_name), &jvm_dir)?;
 
-    jvm_tmp_dir.close().unwrap();
-    send_log(&app_handle, "Downloaded JVM");
+    jvm_tmp_dir.close()?;
+    send_log(&app_handle, "Downloaded JVM")?;
   } else {
-    send_log(&app_handle, "JVM already downloaded");
+    send_log(&app_handle, "JVM already downloaded")?;
   }
 
   let jars_dir = app_local_data_dir.join("jars");
   if !jars_dir.exists() {
-    std::fs::create_dir(&jars_dir).unwrap();
+    std::fs::create_dir(&jars_dir)?;
   }
 
   let soul_fire_version_file =
     jars_dir.join(format!("SoulFireDedicated-{}.jar", soul_fire_version));
   if !soul_fire_version_file.exists() {
-    send_log(&app_handle, "Fetching SoulFire data...");
+    send_log(&app_handle, "Fetching SoulFire data...")?;
     let soul_fire_url = format!("https://github.com/AlexProgrammerDE/SoulFire/releases/download/{}/SoulFireDedicated-{}.jar", soul_fire_version, soul_fire_version);
     info!("SoulFire URL: {}", soul_fire_url);
 
-    fn send_download_progress(app_handle: &AppHandle, progress: u64, total: u64) {
+    fn send_download_progress(app_handle: &AppHandle, progress: u64, total: u64) -> tauri::Result<()> {
       send_log(
         app_handle,
         format!("Downloading SoulFire... {}%", progress * 100 / total),
-      );
+      )
     }
 
-    send_download_progress(&app_handle, 0, 1);
-    let mut response = reqwest::get(&soul_fire_url).await.unwrap();
+    send_download_progress(&app_handle, 0, 1)?;
+    let mut response = reqwest::get(&soul_fire_url).await?;
     let mut content = Vec::new();
-    let total_size = response.content_length().unwrap();
+    let total_size = response.content_length().ok_or(SFError::NoContentLengthHeader)?;
     let mut downloaded_size = 0;
-    while let Some(chunk) = response.chunk().await.unwrap() {
+    while let Some(chunk) = response.chunk().await? {
       downloaded_size += chunk.len() as u64;
       content.extend_from_slice(&chunk);
-      send_download_progress(&app_handle, downloaded_size, total_size);
+      send_download_progress(&app_handle, downloaded_size, total_size)?;
     }
 
-    send_log(&app_handle, "Saving SoulFire...");
-    std::fs::write(&soul_fire_version_file, &content).unwrap();
-    send_log(&app_handle, "Downloaded SoulFire");
+    send_log(&app_handle, "Saving SoulFire...")?;
+    std::fs::write(&soul_fire_version_file, &content)?;
+    send_log(&app_handle, "Downloaded SoulFire")?;
   } else {
-    send_log(&app_handle, "SoulFire already downloaded");
+    send_log(&app_handle, "SoulFire already downloaded")?;
   }
 
-  info!("Integrated Server SoulFire Jar: {}", soul_fire_version_file.to_str().unwrap());
+  info!("Integrated Server SoulFire Jar: {}", soul_fire_version_file.to_str().ok_or(SFError::PathCouldNotBeConverted)?);
 
   let soul_fire_rundir = app_local_data_dir.join("soulfire");
   if !soul_fire_rundir.exists() {
-    std::fs::create_dir(&soul_fire_rundir).unwrap();
+    std::fs::create_dir(&soul_fire_rundir)?;
   }
 
-  send_log(&app_handle, "Starting SoulFire server...");
+  send_log(&app_handle, "Starting SoulFire server...")?;
 
   let java_exec_name = get_java_exec_name();
   let java_bin_dir = jvm_dir.join("bin");
   let java_exec_path = java_bin_dir.join(java_exec_name);
-  let java_exec_path = java_exec_path.to_str().unwrap();
+  let java_exec_path = java_exec_path.to_str().ok_or(SFError::PathCouldNotBeConverted)?;
   info!("Integrated Server Java Executable: {}", java_exec_path);
 
-  let available_port = find_random_available_port().unwrap();
+  let available_port = find_random_available_port().ok_or(SFError::NoPortAvailable)?;
   info!("Integrated Server Port: {}", available_port);
 
   let command = app_handle.shell().command(java_exec_path)
@@ -180,7 +219,7 @@ pub async fn run_integrated_server(
       "-XX:+UseCriticalJavaThreadPriority",
       "-Dsf.flags.v1=true",
       "-jar",
-      soul_fire_version_file.to_str().unwrap(),
+      soul_fire_version_file.to_str().ok_or(SFError::PathCouldNotBeConverted)?,
     ]);
 
   let (mut rx, mut child) = command.spawn().expect("Failed to spawn sidecar");
@@ -188,13 +227,13 @@ pub async fn run_integrated_server(
   // Print all rx messages
   while let Some(message) = rx.recv().await {
     if let Stdout(line) = message {
-      let line = String::from_utf8(line).unwrap();
+      let line = String::from_utf8(line)?;
       let line = strip_ansi_escapes::strip_str(line);
       if line.contains("Finished loading!") {
-        send_log(&app_handle, "Generating token...");
+        send_log(&app_handle, "Generating token...")?;
         break;
       } else {
-        send_log(&app_handle, line);
+        send_log(&app_handle, line)?;
       }
     }
   }
@@ -206,9 +245,9 @@ pub async fn run_integrated_server(
   let token: String = loop {
     if let Some(message) = rx.recv().await {
       if let Stdout(line) = message {
-        let line = String::from_utf8(line).unwrap();
+        let line = String::from_utf8(line)?;
         if line.contains("JWT") {
-          break line.split_whitespace().last().unwrap().to_string();
+          break line.split_whitespace().last().ok_or(SFError::JwtLineInvalid)?.to_string();
         }
       }
     }
