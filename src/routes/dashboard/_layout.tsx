5ebcf37ab4d6d7a30a97cda5e2b71485ec514015
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-10-19 22:57:26 +0200

    Add header on error and pending components

--- src/routes/dashboard/_layout.tsx
+++ src/routes/dashboard/_layout.tsx
@@ -1,5 +1,11 @@
 import { TransportContext } from '@/components/providers/transport-context.tsx';
-import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
+import {
+  createFileRoute,
+  Outlet,
+  redirect,
+  useNavigate,
+  useRouter,
+} from '@tanstack/react-router';
 import { ConfigServiceClient } from '@/generated/soulfire/config.client.ts';
 import { ClientInfoContext } from '@/components/providers/client-info-context.tsx';
 import { createTransport, isAuthenticated } from '@/lib/web-rpc.ts';
@@ -10,10 +16,11 @@ import {
   CardTitle,
 } from '@/components/ui/card.tsx';
 import { LoaderCircleIcon } from 'lucide-react';
-import { ErrorComponent } from '@/components/error-component.tsx';
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
 import { ClientDataResponse } from '@/generated/soulfire/config.ts';
 import demoData from '@/demo-data.json';
+import { DashboardMenuHeader } from '@/components/dashboard-menu-header.tsx';
+import { Button } from '@/components/ui/button.tsx';
 
 export const Route = createFileRoute('/dashboard/_layout')({
   beforeLoad: ({ location }) => {
@@ -54,18 +61,64 @@ export const Route = createFileRoute('/dashboard/_layout')({
     };
   },
   errorComponent: ErrorComponent,
-  pendingComponent: () => (
-    <Card className="m-auto text-center w-full max-w-[450px] border-none">
-      <CardHeader className="pb-0">
-        <CardTitle>Connecting...</CardTitle>
-      </CardHeader>
-      <CardContent className="flex h-32 w-full">
-        <LoaderCircleIcon className="m-auto h-12 w-12 animate-spin" />
-      </CardContent>
-    </Card>
-  ),
+  pendingComponent: PendingComponent,
   component: DashboardLayout,
 });
+
+function ErrorComponent({ error }: { error: Error }) {
+  const navigate = useNavigate();
+  const router = useRouter();
+
+  return (
+    <>
+      <DashboardMenuHeader />
+      <div className="flex flex-grow">
+        <div className="m-auto flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">Error</h1>
+          <p className="text-red-500">{error.message}</p>
+          <div className="flex flex-row gap-2">
+            <Button
+              className="w-fit"
+              onClick={() => {
+                void navigate({
+                  to: '/',
+                  replace: true,
+                });
+              }}
+            >
+              Back to login
+            </Button>
+            <Button
+              onClick={() => {
+                void router.invalidate();
+              }}
+            >
+              Reload page
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function PendingComponent() {
+  return (
+    <>
+      <DashboardMenuHeader />
+      <div className="flex flex-grow">
+        <Card className="m-auto text-center w-full max-w-[450px] border-none">
+          <CardHeader className="pb-0">
+            <CardTitle>Connecting...</CardTitle>
+          </CardHeader>
+          <CardContent className="flex h-32 w-full">
+            <LoaderCircleIcon className="m-auto h-12 w-12 animate-spin" />
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
 
 function DashboardLayout() {
   const { transport, clientData } = Route.useLoaderData();
