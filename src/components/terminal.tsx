8fd87ea9299a4b5a0997c90573d8a6451ce5d64e
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-10-19 20:02:41 +0200

    Fixup cursor sometimes still showing text as selectable

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -158,7 +158,7 @@ export const TerminalComponent = () => {
         } as CSSProperties
       }
     >
-      <div className="whitespace-pre-wrap py-0.5 pl-0.5 h-full leading-3">
+      <div className="whitespace-pre-wrap py-0.5 pl-0.5 h-full leading-3 cursor-text">
         {entries.map((entry) => {
           return (
             <div key={entry[0]} className="py-0.5">
