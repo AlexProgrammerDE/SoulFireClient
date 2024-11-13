e8893fe30f6c853edd8e0f310aee06c345e83f22
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-13 19:07:03 +0100

    Revert "Install module on the fly"
    This reverts commit 6c3c0241336611bff523f7e63b1f32cc7db5ebd8.

--- sf_sign.ps1
+++ sf_sign.ps1
@@ -2,5 +2,4 @@ param (
     [string]$filePath
 )
 
-Install-Module -scope CurrentUser -Force -Name SignPath
 Submit-SigningRequest -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
