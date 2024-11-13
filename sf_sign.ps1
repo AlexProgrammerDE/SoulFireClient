e7b8df7ad70d541608f0c44301b8f8832ef263ae
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-13 20:08:54 +0100

    Try installing for all users

--- sf_sign.ps1
+++ sf_sign.ps1
@@ -2,9 +2,9 @@ param (
     [string]$filePath
 )
 
-Write-Host "Signing $filePath"
+Write-Host "SF starting signing $filePath"
 
 Import-Module SignPath
 Submit-SigningRequest -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
 
-Write-Host "Finished signing $filePath"
+Write-Host "SF finished signing $filePath"
