874996e33a583d15633d5fd4631b9f783700dc93
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-13 19:25:46 +0100

    Import module

--- sf_sign.ps1
+++ sf_sign.ps1
@@ -2,4 +2,5 @@ param (
     [string]$filePath
 )
 
+Import-Module SignPath
 Submit-SigningRequest -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
