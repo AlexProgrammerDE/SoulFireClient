421e0490605af483e3bd1821666472d2388e5042
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-14 19:55:21 +0100

    Remove some useless stuff

--- sf_sign.ps1
+++ sf_sign.ps1
@@ -4,7 +4,6 @@ param (
 
 Write-Host "SF starting signing $filePath"
 
-Import-Module SignPath
 Submit-SigningRequest -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
 
 Write-Host "SF finished signing $filePath"
