d7a63d432068da58d5a675d9439e8b3348e8a706
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-15 21:15:46 +0100

    Catch error

--- sf_sign.ps1
+++ sf_sign.ps1
@@ -4,6 +4,11 @@ param (
 
 Write-Host "SF starting signing $filePath"
 
-# Submit-SigningRequest -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
+try {
+    Submit-SigningRequest -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
+} catch {
+    Write-Host "An error occurred:" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Yellow
+}
 
 Write-Host "SF finished signing $filePath"
