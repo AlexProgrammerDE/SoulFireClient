9f35403186ece3336a4ec6b321171126a9fc80c7
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-15 21:38:45 +0100

    Remove try catch

--- sf_sign.ps1
+++ sf_sign.ps1
@@ -4,11 +4,6 @@ param (
 
 Write-Host "SF starting signing $filePath"
 
-try {
-    Submit-SigningRequest -Force -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
-} catch {
-    Write-Host "An error occurred:" -ForegroundColor Red
-    Write-Host $_.Exception.Message -ForegroundColor Yellow
-}
+Submit-SigningRequest -Force -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
 
 Write-Host "SF finished signing $filePath"
