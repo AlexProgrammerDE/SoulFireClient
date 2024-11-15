06a3ea5658d76e209dee7a7ccda90f11162041f5
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-15 21:25:41 +0100

    Force replace output path

--- sf_sign.ps1
+++ sf_sign.ps1
@@ -5,7 +5,7 @@ param (
 Write-Host "SF starting signing $filePath"
 
 try {
-    Submit-SigningRequest -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
+    Submit-SigningRequest -Force -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
 } catch {
     Write-Host "An error occurred:" -ForegroundColor Red
     Write-Host $_.Exception.Message -ForegroundColor Yellow
