234a9fb833556f26094ad065fb8f07263e02249f
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-12-23 18:14:46 +0100

    Remove PowerShell signing

--- sf_sign.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [string]$filePath
-)
-
-Write-Host "SF starting signing $filePath"
-
-Submit-SigningRequest -Force -InputArtifactPath "$filePath" -ApiToken "$env:SIGNPATH_API_KEY" -OrganizationId "e091b552-3623-4d9d-83d7-059d8f32978b" -ProjectSlug "SoulFireClient" -SigningPolicySlug "$env:SIGNPATH_POLICY_SLUG" -OutputArtifactPath "$filePath" -WaitForCompletion
-
-Write-Host "SF finished signing $filePath"
