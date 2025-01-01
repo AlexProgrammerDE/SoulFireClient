42951ac7702cfdf8b5ccaecce87b4eaa4ada4a72
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-01-01 20:54:40 +0100

    Fix tooltips being 0 ms

--- src/components/nav/instance-page-layout.tsx
+++ src/components/nav/instance-page-layout.tsx
@@ -16,6 +16,7 @@ import { ReactNode, useContext } from 'react';
 import { InstanceInfoContext } from '@/components/providers/instance-info-context.tsx';
 import { InstanceSidebar } from '@/components/nav/instance-sidebar.tsx';
 import { getCookie } from '@/lib/utils.ts';
+import { TooltipProvider } from '@/components/ui/tooltip.tsx';
 
 export default function InstancePageLayout(props: {
   children: ReactNode;
@@ -31,38 +32,40 @@ export default function InstancePageLayout(props: {
       <InstanceSidebar
         expandPluginSettings={props.expandPluginSettings ?? false}
       />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  {instanceInfo.friendlyName}
-                </BreadcrumbItem>
-                {(props.extraCrumbs || []).map((crumb) => (
-                  <>
-                    <BreadcrumbSeparator className="hidden md:block" />
-                    <BreadcrumbItem className="hidden md:block">
-                      {crumb}
-                    </BreadcrumbItem>
-                  </>
-                ))}
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{props.pageName}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <ScrollArea className="h-[calc(100dvh-4rem)] w-full px-4">
-          <div className="flex flex-col min-h-[calc(100dvh-4rem)] w-full">
-            {props.children}
-          </div>
-        </ScrollArea>
-      </SidebarInset>
+      <TooltipProvider delayDuration={500}>
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    {instanceInfo.friendlyName}
+                  </BreadcrumbItem>
+                  {(props.extraCrumbs || []).map((crumb) => (
+                    <>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                      <BreadcrumbItem className="hidden md:block">
+                        {crumb}
+                      </BreadcrumbItem>
+                    </>
+                  ))}
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{props.pageName}</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          <ScrollArea className="h-[calc(100dvh-4rem)] w-full px-4">
+            <div className="flex flex-col min-h-[calc(100dvh-4rem)] w-full">
+              {props.children}
+            </div>
+          </ScrollArea>
+        </SidebarInset>
+      </TooltipProvider>
     </SidebarProvider>
   );
 }
--- src/components/nav/user-page-layout.tsx
+++ src/components/nav/user-page-layout.tsx
@@ -16,6 +16,7 @@ import { ReactNode, useContext } from 'react';
 import { getCookie } from '@/lib/utils.ts';
 import { UserSidebar } from '@/components/nav/user-sidebar.tsx';
 import { ClientInfoContext } from '@/components/providers/client-info-context.tsx';
+import { TooltipProvider } from '@/components/ui/tooltip.tsx';
 
 export default function UserPageLayout(props: {
   children: ReactNode;
@@ -29,42 +30,44 @@ export default function UserPageLayout(props: {
   return (
     <SidebarProvider defaultOpen={defaultOpen}>
       <UserSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                {props.showUserCrumb && (
-                  <>
-                    <BreadcrumbItem className="hidden md:block">
-                      {clientInfo.username}
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator className="hidden md:block" />
-                  </>
-                )}
-                {(props.extraCrumbs || []).map((crumb) => (
-                  <>
-                    <BreadcrumbItem className="hidden md:block">
-                      {crumb}
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator className="hidden md:block" />
-                  </>
-                ))}
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{props.pageName}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <ScrollArea className="h-[calc(100dvh-4rem)] w-full px-4">
-          <div className="flex flex-col min-h-[calc(100dvh-4rem)] w-full">
-            {props.children}
-          </div>
-        </ScrollArea>
-      </SidebarInset>
+      <TooltipProvider delayDuration={500}>
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  {props.showUserCrumb && (
+                    <>
+                      <BreadcrumbItem className="hidden md:block">
+                        {clientInfo.username}
+                      </BreadcrumbItem>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                    </>
+                  )}
+                  {(props.extraCrumbs || []).map((crumb) => (
+                    <>
+                      <BreadcrumbItem className="hidden md:block">
+                        {crumb}
+                      </BreadcrumbItem>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                    </>
+                  ))}
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{props.pageName}</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          <ScrollArea className="h-[calc(100dvh-4rem)] w-full px-4">
+            <div className="flex flex-col min-h-[calc(100dvh-4rem)] w-full">
+              {props.children}
+            </div>
+          </ScrollArea>
+        </SidebarInset>
+      </TooltipProvider>
     </SidebarProvider>
   );
 }
