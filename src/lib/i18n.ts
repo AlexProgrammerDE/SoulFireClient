846b41ae3950397f9d98dd4c62c17a7631490d55
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-01-12 20:04:02 +0100

    Initial work on translations

---
+++ src/lib/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import resourcesToBackend from 'i18next-resources-to-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(
+    resourcesToBackend(
+      (lng: string, ns: string) => import(`../../locales/${lng}/${ns}.json`),
+    ),
+  )
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
