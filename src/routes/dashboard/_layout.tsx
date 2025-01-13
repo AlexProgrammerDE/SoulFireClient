550b53e5b2ca48d463fda02f082ff5c51c2810a3
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-01-13 18:47:49 +0100

    Fix pending component

--- src/routes/dashboard/_layout.tsx
+++ src/routes/dashboard/_layout.tsx
@@ -205,19 +205,16 @@ function PendingComponent() {
   const { t } = useTranslation('common');
 
   return (
-    <>
-      +{' '}
-      <div className="flex flex-grow">
-        <Card className="m-auto text-center w-full max-w-[450px] border-none">
-          <CardHeader className="pb-0">
-            <CardTitle>{t('pending.connecting')}</CardTitle>
-          </CardHeader>
-          <CardContent className="flex h-32 w-full">
-            <LoaderCircleIcon className="m-auto h-12 w-12 animate-spin" />
-          </CardContent>
-        </Card>
-      </div>
-    </>
+    <div className="flex flex-grow">
+      <Card className="m-auto text-center w-full max-w-[450px] border-none">
+        <CardHeader className="pb-0">
+          <CardTitle>{t('pending.connecting')}</CardTitle>
+        </CardHeader>
+        <CardContent className="flex h-32 w-full">
+          <LoaderCircleIcon className="m-auto h-12 w-12 animate-spin" />
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
