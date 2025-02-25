80dc95e93c4a5b3582b6b8664b288ce78b690764
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-02-25 21:18:49 +0100

    Add "not found" view

--- src/routes/dashboard/_layout/instance/$instance/audit-log.tsx
+++ src/routes/dashboard/_layout/instance/$instance/audit-log.tsx
@@ -10,10 +10,12 @@ import { createTransport } from '@/lib/web-rpc.ts';
 import { InstanceServiceClient } from '@/generated/soulfire/instance.client.ts';
 import { queryClientInstance } from '@/lib/query.ts';
 import { LoadingComponent } from '@/components/loading-component.tsx';
+import { SearchXIcon } from 'lucide-react';
+import * as React from 'react';
 import { Card, CardDescription } from '@/components/ui/card.tsx';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { SFTimeAgo } from '@/components/SFTimeAgo';
 import { getGravatarUrl } from '@/lib/utils.tsx';
-import { SFTimeAgo } from '@/components/SFTimeAgo.tsx';
 
 export const Route = createFileRoute(
   '/dashboard/_layout/instance/$instance/audit-log',
@@ -98,40 +100,51 @@ function AuditLog() {
       extraCrumbs={[t('breadcrumbs.controls')]}
       pageName={t('pageName.audit-log')}
     >
-      <div className="grow flex flex-col h-full w-full gap-4">
-        {auditLog.data.auditLog.entry.map((entry) => {
-          const i18nKey = toI18nKey(entry.type);
-          return (
-            <Card
-              className="flex flex-row p-4 items-center h-20 gap-4"
-              key={entry.id}
-            >
-              <Avatar className="rounded-lg">
-                <AvatarImage
-                  src={getGravatarUrl(entry.user!.email)}
-                  alt={entry.user!.username}
-                />
-                <AvatarFallback className="rounded-lg">
-                  {entry.user!.username.slice(0, 2).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col justify-center">
-                <p>
-                  {t(i18nKey, {
-                    username: entry.user!.username,
-                    data: entry.data,
-                  })}
-                </p>
-                <CardDescription>
-                  <SFTimeAgo
-                    date={new Date(parseInt(entry.timestamp!.seconds) * 1000)}
+      {auditLog.data.auditLog.entry.length === 0 ? (
+        <div className="flex flex-1 size-full">
+          <div className="m-auto flex flex-row gap-2">
+            <SearchXIcon className="size-7 m-auto" />
+            <h1 className="text-xl font-bold m-auto">
+              {t('auditLog.noEntries')}
+            </h1>
+          </div>
+        </div>
+      ) : (
+        <div className="grow flex flex-col h-full w-full gap-4">
+          {auditLog.data.auditLog.entry.map((entry) => {
+            const i18nKey = toI18nKey(entry.type);
+            return (
+              <Card
+                className="flex flex-row p-4 items-center h-20 gap-4"
+                key={entry.id}
+              >
+                <Avatar className="rounded-lg">
+                  <AvatarImage
+                    src={getGravatarUrl(entry.user!.email)}
+                    alt={entry.user!.username}
                   />
-                </CardDescription>
-              </div>
-            </Card>
-          );
-        })}
-      </div>
+                  <AvatarFallback className="rounded-lg">
+                    {entry.user!.username.slice(0, 2).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col justify-center">
+                  <p>
+                    {t(i18nKey, {
+                      username: entry.user!.username,
+                      data: entry.data,
+                    })}
+                  </p>
+                  <CardDescription>
+                    <SFTimeAgo
+                      date={new Date(parseInt(entry.timestamp!.seconds) * 1000)}
+                    />
+                  </CardDescription>
+                </div>
+              </Card>
+            );
+          })}
+        </div>
+      )}
     </InstancePageLayout>
   );
 }
